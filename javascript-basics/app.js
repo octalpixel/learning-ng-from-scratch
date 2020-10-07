90de9829d1ba64d789eed6717ce82bc8ae117581
octalpixel <you@example.com>
2020-10-07 20:01:12 +0530

    Week Two

---
+++ javascript-basics/app.js
@@ -0,0 +1,92 @@
+const TODO_ITEMS = []
+const STORAGE_KEY = 'TODO_ITEMS'
+
+console.log(`TODO_ITEMS Initial --> ${TODO_ITEMS.length}`)
+
+
+function saveTodo(todoItem){
+
+    TODO_ITEMS.push(todoItem)
+
+    const localStorageData =  JSON.stringify(TODO_ITEMS)
+
+    localStorage.setItem(STORAGE_KEY, localStorageData )
+    // console.log(`todoItem -->`,todoItem)
+    // console.log(`localStorageData --<`,localStorageData);
+}
+
+
+function loadTodoData(){
+
+    const localData =  localStorage.getItem(STORAGE_KEY) 
+
+    const todoItem  = localData ?  JSON.parse(localData) : []
+
+    TODO_ITEMS.push(...todoItem)
+    
+    loadList()
+
+
+}
+
+function loadList() {
+
+    let listStructure = ''
+
+    for (let index = 0; index < TODO_ITEMS.length; index++) {
+        const todo = TODO_ITEMS[index];
+
+        const listItem = `
+        <div class="todo-item">
+                <div class="todo-title">
+                    ${todo.name}
+                </div>
+                <div class="todo-action">
+                    <button>Mark Complete</button>
+                    <button>Delete</button>
+                </div>
+            </div>
+        `
+
+        listStructure += listItem
+    }
+    document.querySelector("#item-list").innerHTML =  listStructure
+
+}
+
+
+function addTodoItem() {
+    const todoInput = document.querySelector("#todo-title")
+
+    const todoTitle = todoInput.value
+
+    const createdAt = (new Date()).toJSON()
+
+    const todoItem = {
+        name: todoTitle,
+        createdAt,
+        isCompleted: false,
+        isDeleted: false
+    }
+
+    todoInput.value = ''
+
+    saveTodo(todoItem)
+    
+    // loadList()
+
+    // console.log(todoItem)
+}
+
+
+
+
+// 
+
+loadTodoData()
+
+
+
+
+
+
---
+++ javascript-basics/thought_process.md
@@ -0,0 +1,58 @@
+# Where do we start ? 
+1. I need to make the UI 
+Start with mock UI and build late on
+
+2. See where are am storing the data ? 
+Use variables => store as an array of the todo
+
+3. What structure of the storing data ?  What are the fields
+name, isCompleted,isDeleted, created_at
+
+
+
+
+## Process
+
+### Adding Todo
+1. The user enters todo title
+2. The user press save 
+3.  Item gets added to the array of items
+
+### List items
+1. Loops through all the items array
+2. Create html structure
+
+
+### Persist Todo Items
+#### Save
+1. When user creates todo item same to localstorage
+
+#### Load
+1. When page is loaded get data from localstorage
+2. Add data to the variable
+
+
+
+----- Home Works
+
+
+### Delete Data
+1. When the user clicks on the delete button
+2. Check the index of the item 
+3. Remove the item from the array
+4. Load the list
+
+### Delete Data : v2
+1. When the user clicks on the delete button
+2. Check the index of the item 
+3. set isDeleted to true
+4. Gray out delete / have another list for deleted
+5. Load the list
+
+
+### Mark as completed
+1. When the user click on mark as completed
+2. Get the index
+3. set isCompleted to true
+4. Strike out the text
+5. Load list
